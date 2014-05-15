476b1c382507a2d3e5410028608b8d2899fbf50b
Rob Szumlakowski <robsz@pivotallabs.com>
2014-05-15 11:24:43 -0500

    @pivotal-rob-szumlakowski - initial commit

---
+++ OAuth2Client2/PCFViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCFViewController.h
+//  OAuth2Client2
+//
+//  Created by Rob Szumlakowski on 2014-05-15.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCFViewController : UIViewController
+
+@end
---
+++ OAuth2Client2/PCFViewController.m
@@ -0,0 +1,29 @@
+//
+//  PCFViewController.m
+//  OAuth2Client2
+//
+//  Created by Rob Szumlakowski on 2014-05-15.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import "PCFViewController.h"
+
+@interface PCFViewController ()
+
+@end
+
+@implementation PCFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
